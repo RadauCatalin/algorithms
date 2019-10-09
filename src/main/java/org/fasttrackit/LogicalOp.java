b626f8f15818656eb0be534035361368f3515ec3
Catalin <catalin.radau@yahoo.com>
2019-10-09 10:46:30 +0300

    Added Counting to 100 and -100 from a chosen number Counting between 2 chosen numbers Printing even and odd number from 0 to 100

---
+++ src/main/java/org/fasttrackit/LogicalOp.java
@@ -0,0 +1,107 @@
+package org.fasttrackit;
+
+
+import java.util.Scanner;
+
+public class LogicalOp {
+    public void Counting() {
+        int num1;
+        System.out.println("Please insert number for counting to 100 and -100");
+        Scanner scanner = new Scanner(System.in);
+        num1 = scanner.nextInt();
+        System.out.println("Counting to 100: ");
+        for (int i = num1 + 1; i < 101; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+                System.out.print(i + ", ");
+            } else {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Counting to -100: ");
+        for (int i = num1 - 1; i > -101; i--) {
+            if (i % 10 == 0) {
+                System.out.println();
+                System.out.print(i + ", ");
+            } else {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+    public void between() {
+        int num1;
+        int num2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert first number for between counting");
+        num1 = scanner.nextInt();
+        System.out.println("Please insert second number for between counting");
+        num2 = scanner.nextInt();
+        System.out.println("Ascending counting: ");
+        if (num1 > num2) {
+            for (int i = num2 + 1; i < num1; i++) {
+                if (i % 10 == 0) {
+                    System.out.println();
+                    System.out.print(i + ", ");
+                } else {
+                    System.out.print(i + ", ");
+                }
+            }
+        } else {
+            for (int i = num1 + 1; i < num2; i++) {
+                if (i % 10 == 0) {
+                    System.out.println();
+                    System.out.print(i + ", ");
+                } else {
+                    System.out.print(i + ", ");
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Descending counting: ");
+        if (num1 > num2) {
+            for (int i = num1 - 1; i > num2; i--) {
+                if (i % 10 == 0) {
+                    System.out.println();
+                    System.out.print(i + ", ");
+                } else {
+                    System.out.print(i + ", ");
+                }
+            }
+        } else {
+            for (int i = num2 - 1; i > num1; i--) {
+                if (i % 10 == 0) {
+                    System.out.println();
+                    System.out.print(i + ", ");
+                } else {
+                    System.out.print(i + ", ");
+                }
+            }
+        }
+    }
+
+    //Counting 0 to 100
+    //even number and odd number
+    public void Number() {
+        System.out.println("Even number between 0 and 100:");
+        for (int i = 0; i < 101; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+                System.out.print(i + ", ");
+            } else if (i % 2 == 0) {
+                System.out.print(i + ", ");
+            }
+
+        }
+        System.out.println();
+        System.out.println("Odd number between 0 and 100:");
+        for (int i = 0; i < 101; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+            } else if (i % 2 != 0) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}
